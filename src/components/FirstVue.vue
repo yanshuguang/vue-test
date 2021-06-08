7bccd6cb7727b0f54b6d7e944401d01e8c8238dd
yanshuguang <2508788502@qq.com>
2021-06-08 22:47:54 +0800

    init vue

---
+++ src/components/FirstVue.vue
@@ -0,0 +1,61 @@
+<template>
+
+  <div>
+    <h2>输出值：{{ result }}</h2>
+    <button @click="goback">返回</button>
+    <button @click="changeData">改变</button>
+    <h3>{{ message }}</h3>
+    <p>Original message: "{{ message }}"</p>
+    <p>Computed reversed message: "{{ reversedMessage }}"</p>
+
+    <ul>
+      <li v-for="(item,index) in items" :key="item.message">
+        {{ index }}+++++{{ item.message }}
+      </li>
+    </ul>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  // el: '#app-2',
+  name: 'FirstVue',
+  data () {
+    return {
+      result: '我是一颗小虎牙',
+      message: 'test',
+      count: 0,
+      items: [
+        {message: 'Foo'},
+        {message: 'Bar'}
+      ],
+      object: {
+        title: 'How to do lists in Vue',
+        author: 'Jane Doe',
+        publishedAt: '2016-04-10'
+      }
+    }
+  },
+  methods: {
+    goback () {
+      this.$router.back()
+    },
+    changeData () {
+      this.result = '被改变了' + this.count++
+    }
+  },
+  computed: {
+    // 计算属性的 getter
+    reversedMessage: function () {
+      // `this` 指向 vm 实例
+      return this.message.split('').reverse().join('')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="hello">
+    <button @click="gotoNext">下一页</button>
+    <button @click="goback">下一页</button>
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+        >
+          Core Docs
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+        >
+          Forum
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+        >
+          Community Chat
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a
+          href="http://router.vuejs.org/"
+          target="_blank"
+        >
+          vue-router
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vuex.vuejs.org/"
+          target="_blank"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vue-loader.vuejs.org/"
+          target="_blank"
+        >
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+        >
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+  </div>
+  <!--  <div class="hello">-->
+  <!--    <button @click="gotoNext">下一页测试哈哈</button>-->
+  <!--  </div>-->
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome 但事实上所所所所dsssss所所所所所to Your Vue.js App'
+    }
+  },
+  methods: {
+    gotoNext () {
+      console.log('11111')
+      this.$router.push({name: 'FirstVue'})
+    },
+    goback () {
+      this.$router.back()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
