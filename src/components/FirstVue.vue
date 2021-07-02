ebd53141bb6ec3f153dbec3dc1456e6c9b4d755a
yanshuguang <2508788502@qq.com>
2021-07-02 17:35:58 +0800

    init vue

--- src/components/FirstVue.vue
+++ src/components/FirstVue.vue
@@ -2,6 +2,7 @@
 
   <div>
     <h2>输出值：{{ result }}</h2>
+    <input v-model="result">{{ result }}
     <button @click="goback">返回</button>
     <button @click="changeData">改变</button>
     <h3>{{ message }}</h3>
@@ -13,12 +14,44 @@
         {{ index }}+++++{{ item.message }}
       </li>
     </ul>
+    单位：
+    <select v-model="UnitSelected">
+      <option v-for="item in UnitList" :value="item.id" :key="item.id">{{ item.name }}</option>
+    </select>
+    部门：
+    <select v-model="DepartmentSelected">
+      <option v-for="item in DepartmentList" :value="item.id" :key="item.id">{{ item.name }}</option>
+    </select>
+    用户：
+    <select v-model="UserSelected">
+      <option v-for="item in UserList" :value="item.id" :key="item.id">{{ item.name }}</option>
+    </select>
 
   </div>
 
 </template>
 
 <script>
+var UnitRows = [
+  {name: '1', id: '1'},
+  {name: '2', id: '2'}
+]
+var DepartmentRows = [
+  {name: '11', id: '1', pid: '1'},
+  {name: '12', id: '2', pid: '1'},
+  {name: '21', id: '3', pid: '2'},
+  {name: '22', id: '4', pid: '2'}
+]
+var UserRows = [
+  {name: '111', id: '1', pid: '1'},
+  {name: '112', id: '2', pid: '1'},
+  {name: '121', id: '3', pid: '2'},
+  {name: '122', id: '4', pid: '2'},
+  {name: '211', id: '5', pid: '3'},
+  {name: '212', id: '6', pid: '3'},
+  {name: '221', id: '7', pid: '4'},
+  {name: '222', id: '8', pid: '4'}
+]
 export default {
   // el: '#app-2',
   name: 'FirstVue',
@@ -35,7 +68,14 @@ export default {
         title: 'How to do lists in Vue',
         author: 'Jane Doe',
         publishedAt: '2016-04-10'
-      }
+      },
+      UnitSelected: '',
+      UnitList: [],
+      DepartmentSelected: '',
+      DepartmentList: [],
+      UserSelected: '',
+      UserList: []
+
     }
   },
   methods: {
@@ -52,7 +92,38 @@ export default {
       // `this` 指向 vm 实例
       return this.message.split('').reverse().join('')
     }
+  },
+  created: function () {
+    this.UnitList = UnitRows
+    this.UnitSelected = this.UnitList.length > 0 ? this.UnitList[0].id : ''
+
+    var val = this.UnitSelected
+    this.DepartmentList = DepartmentRows.filter(function (x) {
+      return x.pid === val
+    })
+    this.DepartmentSelected = this.DepartmentList.length > 0 ? this.DepartmentList[0].id : ''
+
+    val = this.DepartmentSelected
+    this.UserList = UserRows.filter(function (x) {
+      return x.pid === val
+    })
+    this.UserSelected = this.UserList.length > 0 ? this.UserList[0].id : ''
+  },
+  watch: {
+    UnitSelected: function (val) {
+      this.DepartmentList = DepartmentRows.filter(function (x) {
+        return x.pid === val
+      })
+      this.DepartmentSelected = this.DepartmentList.length > 0 ? this.DepartmentList[0].id : ''
+    },
+    DepartmentSelected: function (val) {
+      this.UserList = UserRows.filter(function (x) {
+        return x.pid === val
+      })
+      this.UserSelected = this.UserList.length > 0 ? this.UserList[0].id : ''
+    }
   }
+
 }
 </script>
 
